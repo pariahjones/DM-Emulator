eab37d5ee0db3c92058a94f806e45b13f6d4db2c
Pariah Jones <pariahplays@gmail.com>
2015-07-11 16:03:58 -0400

    Fixed PRNG
    Fixed compatibility issue with calling system time.

--- DM-Emulator/src/PRNG.java
+++ DM-Emulator/src/PRNG.java
@@ -1,29 +1,27 @@
 
+
 public class PRNG {
 // Psuedo-Random Number Generator
 
 	public static void main(String[] args) {
 		// TODO - Test & Cleanup
 		long InitSeed;
-		long tempSeed;
 		long Seed;
 		long SeedStep1;
 		long SeedStep2;
-		InitSeed = System.currentTimeMillis();
+		
+		InitSeed = System.nanoTime();
 			// Call System Time
-		tempSeed = Math.abs(InitSeed % 0xFFFF);
-			// Conver System Time to positive four byte long
-		SeedStep1 = tempSeed * 0x41C64E6D;
+		SeedStep1 = InitSeed * 1103515245;
 			// Multiply 4 byte long by 1103515245
-		SeedStep2 = SeedStep1 + 0x6073;
+		SeedStep2 = SeedStep1 + 24691;
 			// Adding 24691 to long
-		Seed = SeedStep2 % 0xFFFF;
-			// Result converted to 4 byte long
-		System.out.println("Start: " + Long.toHexString(tempSeed));
-		System.out.println("Step 1: " + Long.toHexString(SeedStep1));
-		System.out.println("Step 2: " + Long.toHexString(SeedStep2));
-		System.out.println("Step 3: ???"); // South Park joke
-		System.out.println("Step 4: " + Long.toHexString(Seed));
+		Seed = Math.abs(SeedStep2) % 65535;
+			// Result converted to 2 byte long
+		System.out.println("Step 1: " + InitSeed);
+		System.out.println("Step 2: ???"); // South Park joke
+		System.out.println("Step 3: " + Seed);
+		System.out.println("Roll 1d20: " + ((Seed % 20) + 1));
 	}
 
 }
